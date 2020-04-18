ad6253bb987e4018d6534376da882bc8662a83ad
Jeff Sellers <Sellerman@Richards-MacBook-Pro.local>
2020-04-17 21:14:11 -0400

    initial - splitting desktop, web, api

---
+++ webfiles/config.py
@@ -0,0 +1,4 @@
+class Config():
+    SECRET_KEY = b';aeirja_)(_9u-a9jdfae90ej-e09!@aldjfa;'
+    SQLALCHEMY_DATABASE_URI = "sqlite:////clockdatabase.sqlite"
+    
---
+++ webfiles/forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, SelectField, StringField
+from wtforms.validators import DataRequired
+
+class MainForm(FlaskForm):
+    start = SubmitField("Start")
+    stop = SubmitField("Stop")
+    adjust = SubmitField("Adjust")
+    report = SubmitField("Report")
+
+class StartForm(FlaskForm):
+    client_list = SelectField(u"Select a Client", validators=[DataRequired()])
+    project_list = SelectField(u"Select a Project", validators=[DataRequired()])
+    start = SubmitField("Start")
+
+class NewForm(FlaskForm):
+    new_client = StringField("client_name")
+    new_project = StringField("client_name")
+    submit = SubmitField("Submit")
+
+    # Custom Validators to check if client or project already exist in the database
