0f55fc31667c7af9e12862cf33a0fb66eb33c754
JSellers0 <JSellers0@Gmail.Com>
2020-05-03 21:46:43 -0400

    remove timelog data from user class

--- models.py
+++ models.py
@@ -19,26 +19,9 @@ class MyUser(UserMixin):
         self.userid = user["userid"]
         self.user_name = user["user_name"]
         self.email = user["email"]
-        self.reset_timelog_data()
         
     @property
     def id(self):
         return self
-
-    def set_timelog_data(self, tl):
-        self.clientid = tl["clientid"]
-        self.client = tl["client_name"]
-        self.projectid = tl["projectid"]
-        self.project = tl["project_name"]
-        self.start = tl["start"]
-
-    def reset_timelog_data(self):
-        self.timelogid = 0
-        self.clientid = 0
-        self.client = ""
-        self.projectid = 0
-        self.project = ""
-        self.start = ""
-        self.stopped = 0
 
 
