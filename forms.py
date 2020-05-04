1342fbe7b139dfb8817e8691c90191771089b690
JSellers0 <JSellers0@Gmail.Com>
2020-05-03 21:45:36 -0400

    fix startform add adjust form

--- forms.py
+++ forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.fields.html5 import DateField
 from wtforms.validators import (DataRequired, Length, Email, EqualTo, ValidationError)
 
 class LoginForm(FlaskForm):
@@ -18,11 +19,18 @@ class RegisterForm(FlaskForm):
     submit = SubmitField("Register")
 
 class StartForm(FlaskForm):
-    client = StringField("Client", validators=[DataRequired()])
     project = StringField("Project", validators=[DataRequired()])
+    task = StringField("Task", validators=[DataRequired()])
+    note = StringField("Note", validators=[DataRequired()])
+    
     submit = SubmitField("Start")
 
 class NewForm(FlaskForm):
     new_client = StringField("New Client")
     new_project = StringField("New Project")
+    submit = SubmitField("Submit")
+
+class AdjustTimeForm(FlaskForm):
+    range_begin = DateField("Beginning Date")
+    range_end = DateField("Ending Date")
     submit = SubmitField("Submit")
