f5f4a5a89d2fb4c59e9cc3cd63993bb3a022757f
JSellers0 <JSellers0@gmail.com>
2020-10-10 11:04:51 -0400

    cred test

--- config.py
+++ config.py
@@ -3,7 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
+# ToDo: Make secret key , DB URI, API URI actually secret.
+
 app = Flask(__name__)
+
 
 app.secret_key = b';aeirja_)(_9u-a9jdfae90ej-e09!@aldjfa;'
 
