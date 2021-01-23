10d6c7ff818b8bac66635ca6c8cd2a0594631859
JSellers0 <JSellers0@Gmail.com>
2021-01-23 12:00:03 -0600

    parameterize connections and keys

--- config.py
+++ config.py
@@ -2,15 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from secerts import mysql, host, ask
 
 # ToDo: Make secret key , DB URI, API URI actually secret.
 
 app = Flask(__name__)
 
 
-app.secret_key = b';aeirja_)(_9u-a9jdfae90ej-e09!@aldjfa;'
+app.secret_key = ask
 
-db_uri = "mysql://clock:" + "M4xG3*@jgOIRdEgmjUfSV" + "@localhost/clock"
+db_uri = "mysql://clock:" + mysql + "@" + host + "/clock"
 
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
@@ -24,4 +25,4 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'danger'
 
-aws_route = "http://192.168.0.202/api/clock"
+aws_route = "http://192.168.0.203/api/clock"
