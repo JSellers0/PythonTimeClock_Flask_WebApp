c6be97f0a28fbf1956d5b53405732c63334ddf40
Sellerman <Jeffrey.Sellers@maritz.com>
2022-03-06 12:54:14 -0500

    switch to dotenv for secrets

--- config.py
+++ config.py
@@ -2,16 +2,20 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from myshit import s_mysql, s_host, s_ask
+from dotenv import load_dotenv
+import os
 
 # ToDo: Make secret key , DB URI, API URI actually secret.
 
 app = Flask(__name__)
 
+load_dotenv
 
-app.secret_key = s_ask
+app.secret_key = os.getenv('PYCLOCK_SK')
 
-db_uri = "mysql+pymysql://clock:" + s_mysql + "@" + s_host + "/clock"
+# ToDo: dev - sqlite
+
+db_uri = f"mysql://{os.getenv('PYCLOCK_MYSQL_USER')}:{os.getenv('PYCLOCK_MYSQL_PASS')}@{os.getenv('PYCLOCK_MYSQL_HOST')}/clock"
 
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
@@ -25,4 +29,4 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'danger'
 
-aws_route = "http://192.168.0.203/api/clock"
+aws_route = "http://192.168.40.100/clock/api"
