86ac8549fa4025b0a8f0b851e03dd6c59e8c9b49
JSellers0 <jsellers0@gmail.com>
2022-03-06 12:53:43 -0500

    wtforms import fix

--- forms.py
+++ forms.py
@@ -1,6 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SelectField, SubmitField, BooleanField
-from wtforms.fields.html5 import DateField
+from wtforms import StringField, PasswordField, SelectField, SubmitField, BooleanField, DateField
 from wtforms.validators import (DataRequired, Length, Email, EqualTo, ValidationError)
 
 class LoginForm(FlaskForm):
